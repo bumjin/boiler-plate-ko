23e523b226509befd1de32b4bd7c33b2a0f0743d
bumjin <bumjin@gmail.com>
2020-04-12 16:49:16 +0900

    회원가입

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
-var ip = require("ip");
-console.log( ip.address() );
+const ip = require("ip");
+const bodyParser = require("body-parser");
+const {User} = require("./models/User");
+
+//application/urlencorded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://bumjin:handy21@boilerplate-gfnfu.mongodb.net/test?retryWrites=true&w=majority', {
@@ -11,7 +18,28 @@ mongoose.connect('mongodb+srv://bumjin:handy21@boilerplate-gfnfu.mongodb.net/tes
 .catch(err => console.log(err))
 
 
+console.log( ip.address() );
+
 app.get('/', (req, res) => res.send('안녕하세요.!'))
+
+app.post('/register', (req, res) => {
+        //회원 가입할 때 필요한 정보들을 client에서 가져오면
+        //그것들을 데이타베이스에 넣어준다.
+        const user = new User(req.body)
+
+        user.save((err, userInfo) => {
+            if(err) return res.json({
+                success: false, err
+            })
+            return res.status(200).json({
+                success: true
+            })
+        })
+    }
+)
+
+
+
 app.listen(port, () => 
     console.log(`Example app listening at http://${ip.address()}:${port}`)
 )
