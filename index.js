cac8f9f76344b5e252733383fe7bdcdc2edabfc5
bumjin <bumjin@gmail.com>
2020-04-15 19:31:10 +0900

    add auth

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const config = require('./config/key')
 const {User} = require("./models/User");
+const {auth} = require("./middleware/auth")
 
 //application/urlencorded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,7 +27,7 @@ console.log( ip.address() );
 
 app.get('/', (req, res) => res.send('굿모닝 안녕하세요.!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/user/register', (req, res) => {
         //회원 가입할 때 필요한 정보들을 client에서 가져오면
         //그것들을 데이타베이스에 넣어준다.
         const user = new User(req.body)
@@ -41,7 +42,7 @@ app.post('/register', (req, res) => {
     }
 )
 
-app.post('/login', (req, res) => {
+app.post('/api/user/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({email: req.body.email}, (err, user) => {
         if(!user) {
@@ -74,6 +75,22 @@ app.post('/login', (req, res) => {
     //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
 
     //비밀번호까지 맞다면 토큰을 생성하기
+})
+
+//role 0 일반 유저, 0이 아니면 관리자
+app.get('/api/user/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True란 말.
+    res.status(200) .json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false: true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+
+    })
 })
 
 
